2c927cfac72171d25a357bdd26299e0de877dd39
Kenneth Reitz <me@kennethreitz.org>
2024-01-30 10:17:13 -0500

    Remove unused import in schema.py

--- neon_client/schema.py
+++ neon_client/schema.py
@@ -5,7 +5,7 @@
 from __future__ import annotations
 
 # from dataclasses import dataclass
-from datetime import date, datetime
+from datetime import datetime
 from enum import Enum
 from typing import Optional
 
