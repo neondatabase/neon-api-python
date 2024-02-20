2e107a3bfa8a0e03258ec72b13c297c424af613f
Kenneth Reitz <me@kennethreitz.org>
2024-02-20 09:10:33 -0500

    Add functions to create NeonAPI instance from environment variables and token

--- neon_client/__init__.py
+++ neon_client/__init__.py
@@ -4,8 +4,12 @@ from .exceptions import NeonAPIError
 
 
 def from_environ():
+    """Create a NeonAPI instance from environment variables."""
+
     return NeonAPI.from_environ()
 
 
 def from_token(token):
+    """Create a NeonAPI instance from a token."""
+
     return NeonAPI.from_token(token)
