61ce56547fab1f48282e59870db77910597d06a3
Felix Wong <felix@waf.hk>
2024-08-09 17:32:10 -0700

    fix database update http method

--- neon_api/client.py
+++ neon_api/client.py
@@ -481,7 +481,7 @@ class NeonAPI:
         """
 
         return self._request(
-            "PUT",
+            "PATCH",
             f"projects/{ project_id }/branches/{ branch_id }/databases/{ database_id }",
             json=json,
         )
